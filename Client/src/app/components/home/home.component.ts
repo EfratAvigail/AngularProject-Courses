b15dd1345c9e2bcc09d7e8192acdcb2b713b183b
EfratAvigail <f0548573828@gmail.com>
2025-03-11 00:23:51 +0200

    after

--- Client/src/app/components/home/home.component.ts
+++ Client/src/app/components/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { MatCard } from '@angular/material/card';
 import { MatIcon } from '@angular/material/icon';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -10,5 +11,9 @@ import { MatIcon } from '@angular/material/icon';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
- 
+  constructor(private router: Router) {}
+
+  signUp() {
+    this.router.navigate(['courses']);
+  }
 }
